c27536a6ccddf098666d49eebd43d20a1d44c2ed
Adam Mulqueen <Adam.Mulqueen@students.ittralee.ie>
2019-11-27 09:45:33 +0000

    hello

---
+++ Hello World.java
@@ -0,0 +1,10 @@
+//My name is Adam Mulqueen
+/* This program sends a simple message "My name is Adam Mulqueen" message to the screen */
+
+public class My name is Adam Mulqueen (
+	public static void main(String args[])(
+			System.out.println("My name is Adam Mulqueen!"); //the message being displayed
+		)
+)
+ 
+		
